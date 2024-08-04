bf18f0671a18748640bc4c6c25ce3909f54cfab7
Alielka0x404 <alielkamili8@gmail.com>
2024-08-04 14:28:57 +0100

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,6 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+read -p 2RvCd5rUVaweD87awdgYvX4SjMc_7JiBEPvcWcSbAbXfS6r1V CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
